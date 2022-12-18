a55c6241e9bc98f749966d1661b2b6d9c509d322
Sudhanshu Kumar <97729053+sudhanshusmith@users.noreply.github.com>
2022-12-19 02:42:53 +0530

    Intial Commit

---
+++ mklMain.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <fstream>
+#include "mkl.cpp"
+#include "pthreads.cpp"
+
+using namespace std;
+using namespace std::chrono;
+
+
+int main(int argc, char **args){
+    
+    
+    string s=args[1];
+    try{
+        if(s.compare("fullyconnected")==0){
+            string type = args[6];
+            if(type.compare("mkl")==0){
+                mklMultiply(args[2],args[3],args[4],args[5]);
+            }else if(type.compare("pthread")==0){
+                pthreadsMultiply(args[2],args[3],args[4],args[5]);
+            }else {
+                cerr<<"wrong argument"<<endl;
+            }
+        }
+    } catch(...){
+        cout << "Wrong inputs" << endl;
+    }
+    return 0;
+}
